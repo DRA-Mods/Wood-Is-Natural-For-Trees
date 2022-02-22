3a1dae4acd173bb6b09ca8552f629f09935ce808
SokyranTheDragon <exploder1531@gmail.com>
2022-02-22 21:48:55 +0100

    Initial commit

---
+++ Source/WoodIsNaturalForTrees/HarmonyPatches.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace WoodIsNaturalForTrees
+{
+    internal static class HarmonyPatches
+    {
+        [HarmonyPatch(typeof(MeditationUtility), nameof(MeditationUtility.CountsAsArtificialBuilding), typeof(Thing))]
+        private static class PatchCountsAsArtificialBuildingThing
+        {
+            [UsedImplicitly]
+            private static void Postfix(Thing t, ref bool __result)
+            {
+                if (!__result || 
+                    t.Stuff?.stuffProps?.categories == null || 
+                    !WoodIsNaturalForTreesMod.settings.stuffMadeFrom.Any()) 
+                    return;
+
+                if (t.Stuff.stuffProps.categories.All(c => WoodIsNaturalForTreesMod.settings.stuffMadeFrom.Contains(c)))
+                    __result = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(MeditationUtility), nameof(MeditationUtility.CountsAsArtificialBuilding), typeof(ThingDef), typeof(Faction))]
+        private static class PatchCountsAsArtificialBuildingDef
+        {
+            [UsedImplicitly]
+            private static void Postfix(ThingDef def, ref bool __result)
+            {
+                if (!__result || 
+                    Find.DesignatorManager.SelectedDesignator is not Designator_Place place ||
+                    place.StuffDef?.stuffProps?.categories == null ||
+                    !WoodIsNaturalForTreesMod.settings.stuffMadeFrom.Any()) 
+                    return;
+
+                if (place.StuffDef.stuffProps.categories.All(c => WoodIsNaturalForTreesMod.settings.stuffMadeFrom.Contains(c)))
+                    __result = false;
+            }
+        }
+    }
+}
---
+++ Source/WoodIsNaturalForTrees/WoodIsNaturalForTreesMod.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+using Verse;
+
+namespace WoodIsNaturalForTrees
+{
+    [UsedImplicitly]
+    public class WoodIsNaturalForTreesMod : Mod
+    {
+        private static Harmony harmony;
+        internal static Harmony Harmony => harmony ??= new Harmony("Dra.WoodIsNaturalForTrees");
+        public static WoodIsNaturalForTreesSettings settings;
+
+        public WoodIsNaturalForTreesMod(ModContentPack content) : base(content)
+        {
+            LongEventHandler.ExecuteWhenFinished(() => settings = GetSettings<WoodIsNaturalForTreesSettings>());
+
+            Harmony.PatchAll();
+
+            if (ModLister.GetActiveModWithIdentifier("Ludeon.RimWorld.Ideology") == null && ModLister.GetActiveModWithIdentifier("Ludeon.RimWorld.Royalty") == null)
+                Log.Error("[WoodIsNaturalForTrees] No supported DLC detected, this mod is pointless");
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect) => settings.DoSettingsWindowContents(inRect);
+
+        public override string SettingsCategory() => "Wood is Natural for Trees";
+    }
+}
